832e00c4a64fe12b3e997d3809cbb30616ff7ac8
Preethi811 <101103530+Preethi811@users.noreply.github.com>
2022-05-19 10:57:08 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,15 @@
+package devops8;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 19;
+		int b = 10;
+		int sum = a+b;
+		System.out.println(sum);
+		
+         
+	}
+
+}
